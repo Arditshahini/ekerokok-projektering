d548b9d17f5a376a80974b8364ea9d83c471c8e8
Ardit.Sh <ardit.shahini89@gmail.com>
2025-04-12 20:38:27 +0200

    removed firebase

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -8,12 +8,12 @@ import { getStorage } from 'firebase/storage'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAdYX_FdL3pF-CvHusRX1pSxSPXiJ3TA0U",
-    authDomain: "iths-crossplatform-23c47.firebaseapp.com",
-    projectId: "iths-crossplatform-23c47",
-    storageBucket: "iths-crossplatform-23c47.appspot.com",
-    messagingSenderId: "798905296625",
-    appId: "1:798905296625:web:2f3dbb9dc5891263fe6b50"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 // Initialize Firebase
